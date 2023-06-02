84557390e538893b18c15f52c219d4c7bcac671f
Alesa <alesa@live.se>
2023-06-02 15:54:14 +0200

    calling the function

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: './index.js', 
     output: {
-        // mode: "development",
         path: path.resolve(__dirname, 'dist'), 
         filename: 'bundle.js', 
     },
